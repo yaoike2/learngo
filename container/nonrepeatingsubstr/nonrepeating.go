75fb41d8c21b0f4c07f18924c829e7d6dbbb4bae
yaoike2 <binemail2000@sina.com>
2018-12-21 18:53:30 +0800

    modified file nonrepeating.go, map version

--- container/nonrepeatingsubstr/nonrepeating.go
+++ container/nonrepeatingsubstr/nonrepeating.go
@@ -4,32 +4,25 @@ import (
 	"fmt"
 )
 
-var lastOccurred = make([]int, 0xffff)
-
 func lengthOfNonRepeatingSubStr(s string) int {
-
-	for i := range lastOccurred {
-		lastOccurred[i] = 0
-	}
-
+	lastOccurred := make(map[rune]int)
 	start := 0
 	maxLength := 0
 
 	for i, ch := range []rune(s) {
-		if lastI := lastOccurred[ch]; lastI >= start {
-			start = lastI
+		if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
+			start = lastI + 1
 		}
 		if i-start+1 > maxLength {
 			maxLength = i - start + 1
 		}
-		lastOccurred[ch] = i + 1
+		lastOccurred[ch] = i
 	}
 
 	return maxLength
 }
 
 func main() {
-
 	fmt.Println(
 		lengthOfNonRepeatingSubStr("abcabcbb"))
 	fmt.Println(
